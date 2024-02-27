2950dee1c1035fb1b5775ce55e9b6f05ee192273
Eemeli Koikkalainen <eemeli.koikkalainen@outlook.com>
2024-02-27 17:14:53 +0200

    configure sst for deployment

---
+++ sst.config.ts
@@ -0,0 +1,20 @@
+import type { SSTConfig } from "sst";
+import { SvelteKitSite } from "sst/constructs";
+
+export default {
+  config(_input) {
+    return {
+      name: "kuvagalleria",
+      region: "eu-north-1",
+      profile: _input.stage === "production" ? "eemeli-production" : "eemeli-dev",
+    };
+  },
+  stacks(app) {
+    app.stack(function Site({ stack }) {
+      const site = new SvelteKitSite(stack, "site");
+      stack.addOutputs({
+        url: site.url,
+      });
+    });
+  },
+} satisfies SSTConfig;
