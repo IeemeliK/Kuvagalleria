b798be2d43f639bef03c12a180f2e6a806b2d908
Eemeli Koikkalainen <eemeli.koikkalainen@outlook.com>
2024-03-15 13:18:40 +0200

    Move signedUrl generation to a function adn fix error handling

--- src/routes/+page.server.js
+++ src/routes/+page.server.js
@@ -2,15 +2,31 @@ import { Bucket } from 'sst/node/bucket';
 import { Api } from 'sst/node/api';
 import { GetObjectCommand, PutObjectCommand, S3Client } from '@aws-sdk/client-s3';
 import { getSignedUrl } from '@aws-sdk/s3-request-presigner';
+import { error } from '@sveltejs/kit';
 
 const client = new S3Client({});
+
+/** 
+ * @param {string} bucket The name of the bucket
+ * @param {string} key The key of the object
+ * @returns {Promise<string>} The signed URL
+ */
+const generateSignedUrl = async (bucket, key) => {
+  const command = new GetObjectCommand({
+    Bucket: bucket,
+    Key: key
+  })
+  const signedUrl = await getSignedUrl(client, command, { expiresIn: 3600 });
+
+  return signedUrl;
+}
 
 /** @type {import('./$types').PageServerLoad} */
 export async function load({ fetch }) {
   const response = await fetch(Api.mongoApi.url)
 
   if (!response.ok) {
-    throw new Error(response.statusText);
+    error(500, response.statusText)
   }
 
   /** @type Array.<{_id: string, imageName:string, imageUrl: string, imageText: string, imageKey: string, urlExpiresIn: number}> */
@@ -26,13 +42,7 @@ export async function load({ fetch }) {
 
   for (const d of data) {
     if (d.urlExpiresIn < Date.now()) {
-      console.log("hello");
-      const command = new GetObjectCommand({
-        Bucket: Bucket.bucket.bucketName,
-        Key: d.imageKey
-      })
-      const signedUrl = await getSignedUrl(client, command, { expiresIn: 3600 });
-
+      const signedUrl = await generateSignedUrl(Bucket.bucket.bucketName, d.imageKey)
       ops.push({ ...d, _id: d._id, imageUrl: signedUrl, urlExpiresIn: Date.now() + 3600000, })
 
       await fetch(Api.mongoApi.url, {
@@ -71,12 +81,7 @@ export const actions = {
 
     if (!response) return { success: false };
 
-    const getCommand = new GetObjectCommand({
-      Bucket: Bucket.bucket.bucketName,
-      Key: imageKey
-    })
-    const signedUrl = await getSignedUrl(client, getCommand, { expiresIn: 3600 });
-
+    const signedUrl = await generateSignedUrl(Bucket.bucket.bucketName, imageKey);
     const imageData = {
       imageName: formData.get('otsikko'),
       imageText: formData.get('kuvateksti'),
@@ -93,9 +98,7 @@ export const actions = {
       body: JSON.stringify(imageData)
     })
 
-    console.log(mongoResponse)
-
-    if (!mongoResponse.ok || mongoResponse.status !== 201) return { success: false };
+    if (!mongoResponse.ok || mongoResponse.status !== 201) error(500, mongoResponse.statusText);
 
     return { success: true };
   }
